f0cb398d63bf3da01f8f4e271fa0b80756eccfe1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-09 12:21:21 +0900

    JS

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// var weaponsList = ["AK-47", "Bazooka", "Napalm"];
+
+// var soldier = {
+//     name: "Nicolás",
+//     gender: "Male",
+//     korean: false,
+//     weapon: weaponsList[2],
+//     shoot: function(bullets){
+//         return "Shooting " + bullets + " bulltes"
+//     },
+//     sayHi: function(){
+//         return "Hello my name is " + name
+//     }
+// }
+
+
+// function add(one, two){
+//     return one+two;
+// }
+
+// function substract(potato, watermelon){
+//     return potato-watermelon;
+// }
+
+// var age = prompt("What is your age?");
+
+
+// if(age<18){
+//     console.log("You can't drink that beer");
+// } else if(age>18 && age<40){
+//     console.log("Cheers");
+// } else if(age>40 && age<60){
+//     console.log("Hey dad");
+// } else {
+//     console.log("Die");
+// }
+
+
+
+
+
+
+
